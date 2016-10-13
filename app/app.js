a87ce1a89dd4b4f1bbac997d025941029f89a591
Paul Cluver <paulcl@sahomeloans.com>
2016-10-13 07:44:23 +0200

    Updated

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 
     "use strict";
 
-    var trainingScheduleApp = angular.module('trainingScheduleApp', ['ui.router', 'dataGrid', 'ngMaterial']);
+    var trainingScheduleApp = angular.module('trainingScheduleApp', ['ui.router', 'dataGrid', 'pagination', 'ngMaterial']);
 
     trainingScheduleApp.constant('VERSION', '0.1');
 
@@ -16,12 +16,11 @@
             });
     });
 
-     trainingScheduleApp.factory('DataSource', ['$http',function($http) {
+    trainingScheduleApp.factory('DataSource', ['$http', function($http) {
         return {
-            get: function(file,callback,transform) {
+            get: function(file, callback, transform) {
                 $http.get(
-                    file,
-                    { transformResponse:transform }
+                    file, { transformResponse: transform }
                 ).
                 success(function(data, status) {
                     callback(data);
@@ -29,26 +28,21 @@
                 error(function(data, status) {
                     console.log("Request failed " + status);
                 });
-           }       
+            }
         }
     }]);
 
-    trainingScheduleApp.controller('appCtrl', ['$scope','DataSource', function($scope, DataSource) {
-        
-        $scope.ShowHide = function(index){
-             $scope.num = index;
-             console.log(index);
-        };
-        
+    trainingScheduleApp.controller('appCtrl', ['$scope', '$mdDialog', 'DataSource', function($scope, $mdDialog, DataSource) {
+
         $scope.gridOptions = {
             data: [],
             urlSync: false
-        }; 
+        };
 
         $scope.statuses = [
             { id: 1, name: 'In Progress' },
             { id: 2, name: 'Not Started' },
-            { id: 3, name: 'Complete' }
+            { id: 3, name: 'Completed' }
         ];
 
         $scope.selectedStatus = { id: 1, name: 'In Progress' };
@@ -57,34 +51,161 @@
 
         var xmlTransform = function(data) {
             var x2js = new X2JS();
-            var json = x2js.xml_str2json( data );
+            var json = x2js.xml_str2json(data);
             return json.Curriculums.Curriculum;
         };
 
         var setData = function(data) {
 
             for (var i = 0; i < data.length; i++) {
-                var oneDay = 24*60*60*1000; 
-                var today = new Date();
-                var startDate = new Date(data[i].StartDate);
+                var today = new moment().format('YYYY-MM-DD');
 
-                if (startDate < today)
-                {
-                    data[i].DoneDays = Math.round(Math.abs((startDate.getTime() - today.getTime())/(oneDay)));
-                    data[i].DoneWeeks = Math.round(Math.abs(data[i].DoneDays / 7));
-                    data[i].RemainingDays = data[i].TotalDays - data[i].DoneDays;
-                    data[i].RemainingWeeks = data[i].TotalWeeks - data[i].DoneWeeks;
-                    data[i].PercentageDone = Math.round(Math.abs(data[i].DoneDays / data[i].TotalDays * 100)) + '%';
-                }
-                $scope.dailyProgram = data[i].DailyProgram;
+                data[i].ID = i + 1;
+                data[i].StartDate = getStartDate(i);
+                data[i].EndDate = getEndDate(i);
+
+                data[i].Status = getStatus();
+                var start = new moment(data[i].StartDate);
+                var end = new moment(data[i].EndDate);
+
+                data[i].TotalDays = end.diff(start, 'days');
+                data[i].TotalWeeks = Math.round(data[i].TotalDays / 7);
+                data[i].DoneDays = getDoneDays();
+                data[i].DoneWeeks = Math.round(Math.abs(data[i].DoneDays / 7));
+                data[i].RemainingDays = data[i].TotalDays - data[i].DoneDays;
+                data[i].RemainingWeeks = data[i].TotalWeeks - data[i].DoneWeeks;
+                data[i].PercentageDone = Math.round(Math.abs(data[i].DoneDays / data[i].TotalDays * 100)) + '%';
+
+                $scope.gridOptions.data.dailyProgramData = data[i].DailyProgram;
             }
 
-            $scope.dataSet = data;
+            $scope.gridOptions.data = data;
+            $scope.showDialog = showDialog;
 
-            
+            function getStartDate(id) {
+                if (id == 0) {
+                    return new moment("2016-09-01").format('YYYY-MM-DD');
+                } else {
+                    return new moment(data[id - 1].EndDate).add(1, 'days').format('YYYY-MM-DD');
+                }
+            }
+
+            function getEndDate(id) {
+                return new moment(data[id].StartDate).add(6, 'months').subtract(1, 'days').format('YYYY-MM-DD');
+            }
+
+            function getDoneDays() {
+                var result = 0;
+                var today = new moment();
+
+                if (start < today) {
+                    var remainingDays = end.diff(today, 'days');
+                    result = data[i].TotalDays - remainingDays;
+                }
+                
+                return result;
+            }
+
+            function getStatus() {
+                var result = "Not Started";
+
+                if (data[i].StartDate < today) {
+                    result = "In Progress"
+                }
+
+                if (data[i].EndDate < today) {
+                    result = "Completed"
+                }
+
+                return result;
+            }
+
+            function showDialog($event, id) {
+                var parentEl = angular.element(document.body);
+                $scope.id = id - 1;
+                $mdDialog.show({
+                    parent: parentEl,
+                    targetEvent: $event,
+                    template: '<style> .parentContainer { display: flex } .parentContainer > div { flex: 1 } </style><md-dialog aria-label="List dialog">' +
+                        '  <md-dialog-content>' +
+                        '   <md-list>' +
+                        '      <md-list-item ng-repeat="item in items">' +
+                        
+                        '       <div class=\'parentContainer\'>'+
+
+                        '       <div class=\'childContainer\'>'+
+                        '       <md-list>' +
+                        '       <strong>DAY</strong><br />' +                        
+                        '        {{ item[\'Day\']}}   '+
+                        '       </md-list>' +
+                        '       </div>' +
+
+                        '       <div class=\'childContainer\'>'+
+                        '       <md-list>' +
+                        '       <strong>STANDING</strong>' +                        
+                        '           <md-list-item ng-repeat="method in item[\'Standing\'][\'Method\']">' +
+                        '               <span>{{method | RemoveChar}}</span>'+
+                        '           </md-list-item>' +
+                        '       </md-list>' +
+                        '       </div>' +
+
+                        '       <div class=\'childContainer\'>'+
+                        '       <md-list>' +
+                        '       <strong>STRIKING</strong>' +                          
+                        '           <md-list-item ng-repeat="method in item[\'Striking\'][\'Method\']">' +
+                        '               {{method | RemoveChar}}'+
+                        '           </md-list-item>' +
+                        '       </md-list>' +
+                        '       </div>' +
+
+                        '       <div class=\'childContainer\'>'+
+                        '       <md-list>' +
+                        '       <strong>TURNING</strong>' +                                                  
+                        '           <md-list-item ng-repeat="method in item[\'Turning\'][\'Method\']">' +
+                        '               {{method | RemoveChar}}'+
+                        '           </md-list-item>' +
+                        '       </md-list>' +                        
+                        '       </div>' +
+
+                        '       <div class=\'childContainer\'>'+
+                        '       <md-list>' +
+                        '       <strong>CHANGING</strong>' +                                                                          
+                        '           <md-list-item ng-repeat="method in item[\'Changing\'][\'Method\']">' +
+                        '               {{method | RemoveChar}}'+
+                        '           </md-list-item>' +
+                        '       </md-list>' +       
+                        '       </div>' +
+
+                        '       </div>' +
+                        '      </md-list-item>' +
+                        '   </md-list>' +
+                        '  </md-dialog-content>' +
+                        '  <md-dialog-actions>' +
+                        '    <md-button ng-click="closeDialog()" class="md-primary">' +
+                        '      Close Dialog' +
+                        '    </md-button>' +
+                        '  </md-dialog-actions>' +
+                        '</md-dialog>',
+                    locals: {
+                        items: $scope.gridOptions.data[$scope.id].DailyProgram,
+                        id: $scope.id
+                    },
+                    controller: DialogController
+                });
+
+                function DialogController($scope, $mdDialog, items, id) {
+                    $scope.items = items;
+                    $scope.id = id;
+                    $scope.closeDialog = function() {
+                        $mdDialog.hide();
+                    }
+                }
+            }
         };
 
-        DataSource.get(SOURCE_FILE,setData,xmlTransform);
+        DataSource.get(SOURCE_FILE, setData, xmlTransform);
+
+
     }]);
 
     trainingScheduleApp.filter('RemoveChar', function() {
